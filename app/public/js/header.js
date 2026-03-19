e4ac446bdd9ccf3a97ac7a84c2e6d1da41d701c5
Ith Chanakijkamjorn <ithcha2525@gmail.com>
2026-03-19 21:30:55 +0000

    Adjusted the Headers and navigation bar for all pages

---
+++ app/public/js/header.js
@@ -0,0 +1,26 @@
+// header.js
+// Usage: renderHeader('index')
+
+function renderHeader(activePage) {
+    const navItems = [
+        { name: 'Home', href: '../html/index.html', id: 'index' },
+        { name: 'Posts', href: '../html/posts.html', id: 'posts' },
+        { name: 'My Posts', href: '../html/my_posts.html', id: 'my_posts' },
+        { name: 'Login', href: '../html/login.html', id: 'login' }
+    ];
+    let navHtml = '<nav class="site-nav"><ul class="site-nav-list">';
+    navItems.forEach(item => {
+        navHtml += `<li><a href="${item.href}" class="site-nav-link${activePage === item.id ? ' active' : ''}">${item.name}</a></li>`;
+    });
+    navHtml += '</ul></nav>';
+        const headerHtml = `
+            <header>
+                <div class="site-header-left">
+                    <a href="../html/index.html"><img src="../imgs/logo.png" alt="Logo" class="logo"></a>
+                    <span class="site-title">Traveller 21</span>
+                </div>
+                ${navHtml}
+            </header>
+        `;
+    document.getElementById('header').innerHTML = headerHtml;
+}
